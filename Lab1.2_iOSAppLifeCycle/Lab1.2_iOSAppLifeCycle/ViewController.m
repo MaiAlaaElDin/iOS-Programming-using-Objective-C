9c5a7aff5de8d178abeb98a865d39268219171e0
Mai Alaa El Din <38223586+MaiAlaaElDin@users.noreply.github.com>
2019-04-11 17:48:13 +0200

    iOS App LifeCycle Demo
    Callback functions of iOS app lifecycle.

---
+++ Lab1.2_iOSAppLifeCycle/Lab1.2_iOSAppLifeCycle/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Lab1.2_iOSAppLifeCycle
+//
+//  Created by JETS Mobile Lab on 4/11/19.
+//  Copyright © 2019 MaiAlaa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
